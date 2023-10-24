5d9b620367dfeb1385071bc3653ddd89483b975d
Kadri <knizam@psu.edu>
2023-10-24 16:24:09 -0400

    test_query and test_flight

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,66 +1,27 @@
-import datetime
-
-import pytest
-
-from google_flight_scraper.flight import FlightDetail
-
-_TODAY_DATE = datetime.datetime.now().strftime("%Y-%m-%d")
+from google_flight_scraper.query import FlightQuery, TravelPlan
 
 
-def test_invalid_departure_date_format():
-    with pytest.raises(ValueError, match="Invalid departure date format."):
-        FlightDetail(
-            origin="SFO",
-            destination="LAX",
-            departure_date="2023-15-40",
-            num_adults=3,
-            num_children=1,
-        )
+def test_multiple_departure_and_return_dates():
+    queries = FlightQuery.from_travel_plans(
+        TravelPlan(
+            "LAX",
+            "SFO",
+            departure_dates=["2023-12-25", "2023-12-26", "2023-12-27"],
+            return_dates=["2023-12-30", "2023-12-31"],
+        ),
+    )
+
+    assert len(queries) == 6
 
 
-def test_invalid_return_date_format():
-    with pytest.raises(ValueError, match="Invalid return date format."):
-        FlightDetail(
-            origin="SFO",
-            destination="LAX",
-            departure_date=_TODAY_DATE,
-            return_date="12/12/2023",
-            num_adults=3,
-            num_children=1,
-        )
+def test_multiple_travel_plans():
+    queries = FlightQuery.from_travel_plans(
+        TravelPlan(
+            "LAX", "SFO", ["2023-12-25", "2023-12-26", "2023-12-27", "2023-12-28"]
+        ),
+        TravelPlan(
+            "SFO", "ORD", ["2023-12-30", "2023-12-31"], ["2024-01-01", "2024-01-02"]
+        ),
+    )
 
-
-def test_departure_date_in_the_past():
-    with pytest.raises(ValueError, match="Departure date is in the past."):
-        FlightDetail(
-            origin="SFO",
-            destination="LAX",
-            departure_date="2020-10-02",
-            num_adults=3,
-            num_children=1,
-        )
-
-
-def test_return_date_in_the_past():
-    with pytest.raises(ValueError, match="Return date is before departure date."):
-        FlightDetail(
-            origin="SFO",
-            destination="LAX",
-            departure_date=_TODAY_DATE,
-            return_date="2023-09-02",
-            num_adults=3,
-            num_children=1,
-        )
-
-
-def test_invalid_airport_code():
-    with pytest.raises(
-        ValueError, match="Both origin and destination must be IATA airport codes."
-    ):
-        FlightDetail(
-            origin="NONE",
-            destination="LAX",
-            departure_date=_TODAY_DATE,
-            num_adults=3,
-            num_children=1,
-        )
+    assert len(queries) == 8
